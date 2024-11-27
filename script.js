392d081243b725709673321631415e89fe0632cb
George Leslie <georgeleslie06@gmail.com>
2024-11-27 07:55:10 +0700

    test

--- script.js
+++ script.js
@@ -115,3 +115,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var typingEffect = new Typed(".typedText", {
+        strings: ["Designer", "Coder", "Developer"],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000,
+    });
+});
+
+ 
