3f6174182e2c89720d61196f218b317d9c1025b9
George Leslie <georgeleslie06@gmail.com>
2024-11-29 12:30:42 +0700

    Update contact form backend URL with Render deployment

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ async function handleContactFormSubmit(event) {
     };
 
     try {
-        const response = await fetch('https://your-render-url/send-email', {
+        const response = await fetch('https://portfolio-contact-form-bdzb.onrender.com/send-email', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
